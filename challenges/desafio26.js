b2a5a4496982cfe5344caee73fd4dd3368235d56
nato-re <alvares.renato@hotmail.com>
2020-12-11 11:45:49 -0300

    globals

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,7 +1,7 @@
 db.voos.deleteMany({
   $and: [
-    { "empresa.nome": "GOL" },
-    { "passageiros.pagos": { $gte: 5 } },
-    { "passageiros.pagos": { $lte: 10 } },
+    { 'empresa.nome': 'GOL' },
+    { 'passageiros.pagos': { $gte: 5 } },
+    { 'passageiros.pagos': { $lte: 10 } },
   ],
 });
