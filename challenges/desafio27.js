add594b93bf7e253b553be8d804065de944901bf
Renato Filho <renatosousafilho@gmail.com>
2021-01-17 17:51:01 -0300

    desafios 27 e 28

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -6,6 +6,18 @@
 
 // Para isso, escreva no arquivo desafio27.js duas queries, nesta ordem:
 
-// Conte quantos voos da empresa PASSAREDO cujo campo natureza possua valor igual a Doméstica e crie uma query que insira na coleção resumoVoos um documento com os campos: empresa (nome da empresa) e totalVoosDomesticos (o total retornado anteriormente).
+// Conte quantos voos da empresa PASSAREDO cujo campo natureza possua valor igual a Doméstica 
+// e crie uma query que insira na coleção resumoVoos um documento com os campos: 
+// empresa (nome da empresa) e totalVoosDomesticos (o total retornado anteriormente).
 
-// Em uma segunda query, retorne a empresa e o totalVoosDomesticos do primeiro documento presente na coleção resumoVoos em que a empresa seja PASSAREDO.
+db.resumoVoos.insertOne({
+  empresa: 'PASSAREDO',
+  totalVoosDomesticos: db.voos.count({
+    'empresa.nome': 'PASSAREDO',
+    natureza: 'Doméstica'
+  })
+})
+
+// Em uma segunda query, retorne a empresa e o totalVoosDomesticos do primeiro documento presente 
+// na coleção resumoVoos em que a empresa seja PASSAREDO.
+db.resumoVoos.findOne({}, {empresa: 1, totalVoosDomesticos: 1, _id: false});
--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -3,6 +3,17 @@
 
 // Para isso, escreva no arquivo desafio28.js duas queries, nesta ordem:
 
-// Conte quantos voos da empresa LATAM AIRLINES BRASIL cujo campo natureza possua valor igual a Doméstica e crie uma query que insira na coleção resumoVoos um documento com os campos: empresa (nome da empresa) e totalVoosDomesticos (o total retornado anteriormente).
+// Conte quantos voos da empresa LATAM AIRLINES BRASIL cujo campo natureza possua 
+// valor igual a Doméstica e crie uma query que insira na coleção resumoVoos um documento 
+// com os campos: empresa (nome da empresa) e totalVoosDomesticos (o total retornado anteriormente).
+db.resumoVoos.insertOne({
+  empresa: 'LATAM AIRLINES BRASIL',
+  totalVoosDomesticos: db.voos.count({
+    'empresa.nome': 'LATAM AIRLINES BRASIL',
+    natureza: 'Doméstica'
+  })
+})
+
 
 // Em uma segunda query, retorne a empresa e o totalVoosDomesticos do primeiro documento presente na coleção resumoVoos em que a empresa seja LATAM AIRLINES BRASIL.
+db.resumoVoos.findOne({}, {empresa: 1, totalVoosDomesticos: 1, _id: false});
