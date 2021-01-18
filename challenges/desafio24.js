2fded75f7bb1c81aec67130426f544f0afaf5573
Rodrigo G. Oliveira <rodrigo.gargani@betrybe.com>
2021-01-18 11:13:56 -0300

    testando a pr

---
+++ challenges/desafio24.js
@@ -0,0 +1,11 @@
+db.voos
+  .find(
+    {
+      $and: [
+        { litrosCombustivel: { $not: { $gt: 600 }, $exists: true } },
+        { $nor: [{ "empresa.nome": "GOL" }, { "empresa.nome": "AZUL" }] },
+      ],
+    },
+    { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 }
+  )
+  .limit(1);
